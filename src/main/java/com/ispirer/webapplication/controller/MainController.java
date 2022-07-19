1e21be8e37b9f30c0d28d4645d2af34d20183cbc
Alexey <virtualexus@gmail.com>
2022-07-19 22:28:57 +0300

    logic update

--- src/main/java/com/ispirer/webapplication/controller/MainController.java
+++ src/main/java/com/ispirer/webapplication/controller/MainController.java
@@ -1,14 +1,13 @@
 package com.ispirer.webapplication.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value = "/controller")
+
+@Controller
 public class MainController {
     @GetMapping
     public String retrieveMainInfo(){
-        return "Ispirer test task";
+        return "index";
     }
 }
